ceb26c52f41182b4c8d6a32d161781f1a365380a
MILGAM <93873127+Milgam06@users.noreply.github.com>
2026-01-29 04:08:45 +0900

    feat: add blurBox component

---
+++ components/layouts/blurBox.component.tsx
@@ -0,0 +1,30 @@
+import { BlurView } from 'expo-blur';
+import { memo, ReactNode } from 'react';
+import { Stack } from 'tamagui';
+
+type IBlurBoxProps = {
+  children: ReactNode;
+};
+
+export const BlurBox = memo<IBlurBoxProps>(({ children }) => {
+  return (
+    <BlurView
+      style={{
+        width: '100%',
+        height: 'auto',
+        borderRadius: 12,
+        overflow: 'hidden',
+        borderWidth: 0.6,
+        borderColor: '#858090',
+        boxShadow: '0 0 8px #858090',
+      }}
+      intensity={80}
+      tint="dark">
+      <Stack width="$fluid" px="$size.x6" py="$size.x4">
+        {children}
+      </Stack>
+    </BlurView>
+  );
+});
+
+BlurBox.displayName = 'BlurBox';
