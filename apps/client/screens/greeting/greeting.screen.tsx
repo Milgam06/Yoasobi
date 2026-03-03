d3871a80eb98ffcb3d39c7001bd6684b809af80c
MILGAM <93873127+Milgam06@users.noreply.github.com>
2026-03-04 05:06:37 +0900

    feat(front): style greeting screen

--- apps/client/screens/greeting/greeting.screen.tsx
+++ apps/client/screens/greeting/greeting.screen.tsx
@@ -1,32 +1,92 @@
-import { BlurBox, Header } from '@/components';
-import { memo } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Stack, Text } from 'tamagui';
+import { BlurBox, LinearGradientLayout } from '@/components';
+import { useRouter } from 'expo-router';
+import { memo, useCallback } from 'react';
+import { Button, Stack, Text } from 'tamagui';
 
 export const GreetingScreen = memo(() => {
+  const route = useRouter();
+  const handlePressStart = useCallback(() => {
+    route.push('/auth/auth');
+  }, [route]);
+
+  const handlePressLogin = useCallback(() => {
+    route.push('/auth/auth');
+  }, [route]);
+
   return (
-    <SafeAreaView edges={['top', 'bottom']} style={{ flex: 1 }}>
-      <Header />
-      <Stack flex={1} justify="space-between" items="center" px="$size.x4">
-        <Stack width="$fluid" justify="center" gap="$size.x3">
-          <Stack gap="$size.x1_5">
-            <Text fontSize="$10" fontWeight="$900" color="$colors.lampYellow">
+    <LinearGradientLayout hasHeader>
+      <Stack flex={1} justify="space-between" items="center" px="$size.x2" pt="$size.x15">
+        <Stack width="$fluid" justify="center" gap="$size.x7">
+          <Stack gap="$size.x2">
+            <Text
+              fontSize="$10"
+              fontWeight="$900"
+              color="$colors.lampYellow"
+              shadowColor="$colors.lampYellow"
+              shadowOpacity={1}
+              shadowRadius={16}
+              shadowOffset={{ width: 0, height: 2 }}>
               YOASOBI
             </Text>
-            <Text fontSize="$6" fontWeight="$400" color="$colors.cloudGray">
-              일주일에 단 한 번, 새벽을 빌려요.
+            <Text fontSize="$8" fontWeight="$300" color="$colors.moonSoftWhite">
+              일주일에 단 한 번, 새벽을 빌릴게요.
             </Text>
           </Stack>
           <BlurBox>
             <Stack gap="$size.x1">
-              <Text fontSize="$5" color="$colors.moonSoftWhite">
+              <Text fontSize="$6" color="$colors.moonSoftWhite">
                 가볍게 시작해요.
               </Text>
-              <Text color="$colors.cloudGray">요일을 정하고, 새벽에 산책해봐요. 부담없이 짧게.</Text>
+              <Text fontSize="$5" color="$colors.cloudGray">
+                요일을 정하고, 새벽에 산책해봐요.
+              </Text>
             </Stack>
           </BlurBox>
         </Stack>
+        <Stack width="$fluid" items="center" gap="$size.x3">
+          <Button
+            width="$fluid"
+            height="auto"
+            bg="$colors.moonSoftWhite"
+            py="$size.x5"
+            borderTopLeftRadius="$size.x5"
+            borderTopRightRadius="$size.x5"
+            borderBottomLeftRadius="$size.x5"
+            borderBottomRightRadius="$size.x5"
+            pressStyle={{
+              bg: '$colors.moonSoftWhite',
+              opacity: 0.8,
+            }}
+            onPress={handlePressStart}>
+            <Text fontSize="$8" fontWeight="$800" color="$colors.midnightPurple">
+              시작하기
+            </Text>
+          </Button>
+          <Button
+            width="$fluid"
+            height="auto"
+            bg="$colors.midnightPurple"
+            borderWidth={1}
+            borderColor="$colors.cloudGray"
+            py="$size.x5"
+            borderTopLeftRadius="$size.x5"
+            borderTopRightRadius="$size.x5"
+            borderBottomLeftRadius="$size.x5"
+            borderBottomRightRadius="$size.x5"
+            pressStyle={{
+              bg: '$colors.midnightPurple',
+              borderWidth: 1,
+              borderColor: '$colors.cloudGray',
+              opacity: 0.8,
+            }}
+            onPress={handlePressLogin}>
+            <Text fontSize="$8" fontWeight="$800" color="$colors.moonSoftWhite">
+              로그인
+            </Text>
+          </Button>
+          <Text color="$colors.cloudGray">시작하기를 누르면 회원가입 화면으로 이동해요.</Text>
+        </Stack>
       </Stack>
-    </SafeAreaView>
+    </LinearGradientLayout>
   );
 });
