c2bad438fcebf472a6abcdf46af9174dd030decf
MILGAM <93873127+Milgam06@users.noreply.github.com>
2026-03-04 12:46:58 +0900

    feat(front): get AuthType props at auth screen

--- apps/client/screens/auth/auth.screen.tsx
+++ apps/client/screens/auth/auth.screen.tsx
@@ -1,17 +1,36 @@
-import { DefaultLayout } from '@/components';
-import { memo } from 'react';
-import { Stack, Text } from 'tamagui';
+import { BlurBox, LinearGradientLayout } from '@/components';
+import { memo, useState } from 'react';
+import { ScrollView, Stack, Text } from 'tamagui';
+
+type IAuthType = 'login' | 'signUp';
 
 export type IAuthScreenProps = {
-  authType: 'login' | 'signUp';
+  authType: IAuthType;
 };
 
 export const AuthScreen = memo<IAuthScreenProps>(({ authType }) => {
+  const [currentAuthType, setCurrentAuthType] = useState<IAuthType>(authType);
   return (
-    <DefaultLayout isBlur hasHeader>
-      <Stack flex={1}>
-        <Text>{authType}</Text>
-      </Stack>
-    </DefaultLayout>
+    <LinearGradientLayout hasHeader>
+      <ScrollView flex={1}>
+        <Stack flex={1} items="center" px="$size.x2">
+          <Stack width="$fluid" gap="$size.x2">
+            <Text fontSize="$5" fontWeight="$600" color="$colors.moonSoftWhite">
+              계정
+            </Text>
+            <BlurBox>
+              <Stack flexDirection="row">
+                <Stack flex={1}>
+                  <Text>아이디</Text>
+                </Stack>
+                <Stack flex={1}>
+                  <Text>비밀번호</Text>
+                </Stack>
+              </Stack>
+            </BlurBox>
+          </Stack>
+        </Stack>
+      </ScrollView>
+    </LinearGradientLayout>
   );
 });
