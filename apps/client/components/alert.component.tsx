c6f3f107eb500dcf52d64d2b5025955114d6a071
MILGAM <93873127+Milgam06@users.noreply.github.com>
2026-04-16 01:49:48 +0900

    fix(front): add bg props at alert component

--- apps/client/components/alert.component.tsx
+++ apps/client/components/alert.component.tsx
@@ -1,38 +1,43 @@
 import { memo, ReactNode, useCallback } from 'react';
+import { OpaqueColorValue } from 'react-native';
 import { AlertDialog, GetThemeValueForKey, Stack } from 'tamagui';
 
 type IAlertProps = {
   isOpen: boolean;
   onClose: () => void;
   alertPadding?: GetThemeValueForKey<'padding'> | number;
+  bg?: GetThemeValueForKey<'backgroundColor'> | OpaqueColorValue;
   children: ReactNode;
 };
 
-export const Alert = memo<IAlertProps>(({ isOpen, onClose, alertPadding = '$size.x2', children }) => {
-  const handlePressCloseAlert = useCallback(() => {
-    onClose();
-  }, [onClose]);
-  return (
-    <AlertDialog open={isOpen}>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay
-          key="overlay"
-          animation="200ms"
-          enterStyle={{ opacity: 1 }}
-          exitStyle={{ opacity: 0 }}
-          bg="rgba(0,0,0,0.4)"
-          onPress={handlePressCloseAlert}
-        />
-        <AlertDialog.Content
-          radiused={false}
-          p={alertPadding}
-          enterStyle={{
-            scale: 0.98,
-          }}
-          animation="200ms">
-          <Stack>{children}</Stack>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog>
-  );
-});
+export const Alert = memo<IAlertProps>(
+  ({ isOpen, onClose, alertPadding = '$size.x2', bg = '$colors.midnightPurple', children }) => {
+    const handlePressCloseAlert = useCallback(() => {
+      onClose();
+    }, [onClose]);
+    return (
+      <AlertDialog open={isOpen}>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay
+            key="overlay"
+            animation="200ms"
+            enterStyle={{ opacity: 1 }}
+            exitStyle={{ opacity: 0 }}
+            bg="rgba(0,0,0,0.4)"
+            onPress={handlePressCloseAlert}
+          />
+          <AlertDialog.Content
+            bg={bg}
+            radiused={false}
+            p={alertPadding}
+            enterStyle={{
+              scale: 0.98,
+            }}
+            animation="200ms">
+            <Stack>{children}</Stack>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog>
+    );
+  },
+);
